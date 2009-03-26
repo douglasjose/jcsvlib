d931839f87518994e790634f6e47051464979bed
douglasjose <douglasjose@63876a2a-1a45-11de-9c9a-2d325a3e6494>
2009-03-26 20:41:15 +0000

    Initial import

---
+++ trunk/src/main/java/com/douglasjose/csv/AbstractCsvParser.java
@@ -0,0 +1,129 @@
+package com.douglasjose.csv;
+
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Skeleton interface class
+ *
+ * @author Douglas Rodrigues
+ */
+public abstract class AbstractCsvParser implements CsvParser {
+
+    private final char[] CHAR_TEXT_DELIMITER = this.getTextDelimiter().toCharArray();
+    private final char[] CHAR_FIELD_SEPARATOR = this.getFieldSeparator().toCharArray();
+
+    private String escapeCell(String content) {
+        String out = content;
+        boolean hasFieldSeparator = content.indexOf(this.getFieldSeparator()) > -1;
+        boolean hasTextDelimiter = content.indexOf(this.getTextDelimiter()) > -1;
+        if (hasTextDelimiter) {
+            // The text delimiter is escaped (duplicated)
+            out = out.replaceAll(this.getTextDelimiter(),
+                    this.getTextDelimiter() + this.getTextDelimiter());
+        }
+        if (hasFieldSeparator || hasTextDelimiter) {
+            // Put the content between the text delimiter
+            out = this.getTextDelimiter() + out + this.getTextDelimiter();
+        }
+        return out;
+    }
+
+    private String unescapeCell(String escapedContent) {
+        String out = escapedContent;
+        if (out.indexOf(this.getTextDelimiter()) > -1) {
+            // Remove surrounding text delimiter, in first and last positions
+            int dLength = this.getTextDelimiter().length();
+            out = out.substring(dLength, out.length() - dLength);
+
+            // Replace all duplicated delimiters
+            out = out.replaceAll(this.getTextDelimiter() + this.getTextDelimiter(),
+                    this.getTextDelimiter());
+        }        
+        return out;
+    }
+
+    public void writeFile(Csv csv, OutputStream os) throws IOException {
+        PrintWriter pw = new PrintWriter(os);
+        for (int i = 0; i < csv.getRows(); i++) {
+            for (int j = 0; j < csv.getColumns(); j++) {
+                pw.print(escapeCell(csv.get(i,j)));
+                if (j < csv.getColumns() - 1) {
+                    pw.print(this.getFieldSeparator());
+                }
+            }
+            pw.println();
+        }        
+        pw.close();
+        os.close();
+    }
+
+    public void loadFile(Csv csv, InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line;
+        int lineNumber = 0;
+        int columnNumber;
+        while ((line = reader.readLine()) != null) {
+            List<String> fields = this.splitLine(line);
+            columnNumber = 0;
+            for (String field : fields) {
+                csv.add(lineNumber, columnNumber++, this.unescapeCell(field));
+            }
+            lineNumber++;
+        }
+        reader.close();
+        is.close();
+    }
+
+    private List<String> splitLine(String line) {
+        List<String> out = new ArrayList<String>();
+        // If the current position points to a literal (within text delimiter) or not
+        boolean literal = false;
+        char[] cLine = line.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < line.length(); i++) {
+            if (!literal && startsWith(cLine, CHAR_FIELD_SEPARATOR, i)) {
+                out.add(sb.toString());
+                sb = new StringBuffer();
+                i += CHAR_FIELD_SEPARATOR.length - 1; // Skipping
+            } else if (startsWith(cLine, CHAR_TEXT_DELIMITER, i)) {
+                // Text delimiter found, flip the literal flag
+                literal = !literal;
+                i += CHAR_TEXT_DELIMITER.length - 1; // Skipping
+            } else {
+                sb.append(cLine[i]);
+            }
+        }
+        String last = sb.toString();
+        if (last != null && last.length() > 0) {
+            out.add(last);
+        }        
+        return out;
+    }
+
+    /**
+     * Checks for the presence of a char array (<code>pattern</code>) as a subsequence in another
+     * char array (<code>buffer</code>) starting from the position <code>offset</code>. 
+     * @param buffer
+     * @param pattern
+     * @param offset
+     * @return
+     */
+    private boolean startsWith(char[] buffer, char[] pattern, int offset) {
+        for (int i = 0; i < pattern.length && offset + i < buffer.length; i++) {
+            if (buffer[offset + i] != pattern[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Regular expression used to separate cells in the file
+     * @return
+     */
+    protected abstract String getFieldSeparator();
+
+    protected abstract String getTextDelimiter();
+}
---
+++ trunk/src/main/java/com/douglasjose/csv/CsvParser.java
@@ -0,0 +1,19 @@
+package com.douglasjose.csv;
+
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * @author Douglas Rodrigues
+ */
+public interface CsvParser {
+
+    //public String escapeCell(String content);
+
+    //public String unescapeCell(String escapedContent);
+
+    public void writeFile(Csv csv, OutputStream os) throws IOException;
+
+    public void loadFile(Csv csv, InputStream is) throws IOException;
+}
